0ce4cd5b84c679ce09fd426846684e59af6deb73
ja1616 <ja1616@ic.ac.uk>
2018-03-01 19:22:43 +0000

    Added printing solution for each city

--- src/hashcode/Solution.java
+++ src/hashcode/Solution.java
@@ -1,11 +1,22 @@
 package hashcode;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
 
   public static void main(String[] args) {
-    City city = Parser.parseCity("input_data/a_example.in");
-//    RideAssignment assignment = algo.getAssignment(city);
-//    System.out.println(assignment.outputSolution());
+    City cityA = Parser.parseCity("input_data/a_example.in");
+    City cityB = Parser.parseCity("input_data/b_should_be_easy.in");
+    City cityC = Parser.parseCity("input_data/c_no_hurry.in");
+    City cityD = Parser.parseCity("input_data/d_metropolis.in");
+    City cityE = Parser.parseCity("input_data/e_high_bonus.in");
+    List<City> cities = Arrays.asList(cityA, cityB, cityC, cityD, cityE);
+
+    for (City city : cities) {
+      RideAssignment assignment = alngo.getAssignment(city);
+      System.out.println(assignment.outputSolution());
+    }
   }
 
 }
